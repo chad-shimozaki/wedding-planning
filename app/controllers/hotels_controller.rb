8993a3b9834e2011475c6748ca93068a5b17d199
chad-shimozaki <chad.shimozaki@chicagobooth.edu>
2024-12-05 06:05:27 +0000

    updates to DJ and Hotel controller and view templates

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -18,7 +18,7 @@ class HotelsController < ApplicationController
 
     @list_of_neighborhoods = Neighborhood.all.order({ :name => :desc })
 
-    @the_florist_neighborhood = Neighborhood.where({ :id => @the_florist.neighborhood_id }).at(0)
+    @the_hotel_neighborhood = Neighborhood.where({ :id => @the_hotel.neighborhood_id }).at(0)
 
     render({ :template => "hotels/show" })
   end
@@ -26,34 +26,37 @@ class HotelsController < ApplicationController
   def create
     the_hotel = Hotel.new
     the_hotel.name = params.fetch("query_name")
-    the_hotel.website = params.fetch("query_website")
     the_hotel.address = params.fetch("query_address")
 
         #lat & lng
+        if the_hotel.address != ""
+          maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + the_hotel.address + "&key=" + ENV.fetch("GMAPS_KEY")
 
-        maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + the_hotel.address + "&key=" + ENV.fetch("GMAPS_KEY")
-
-        resp = HTTP.get(maps_url)
-        raw_response = resp.to_s
-        parsed_response = JSON.parse(raw_response)
-  
-        results = parsed_response.fetch("results")
-        first_result = results.at(0)
-        geo = first_result.fetch("geometry")
-        loc = geo.fetch("location")
-  
-        the_hotel.lat = loc.fetch("lat")
-        the_hotel.lng = loc.fetch("lng")
+          resp = HTTP.get(maps_url)
+          raw_response = resp.to_s
+          parsed_response = JSON.parse(raw_response)
+    
+          results = parsed_response.fetch("results")
+          first_result = results.at(0)
+          geo = first_result.fetch("geometry")
+          loc = geo.fetch("location")
+    
+          the_hotel.lat = loc.fetch("lat")
+          the_hotel.lng = loc.fetch("lng")
+        end
  
+    neighborhood_name = params.fetch("query_neighborhood_name")
+    the_hotel.neighborhood_id = Neighborhood.where({ :name => neighborhood_name }).at(0).id
+
+    the_hotel.website = params.fetch("query_website")
+    the_hotel.contact_name = params.fetch("query_contact_name")
+    the_hotel.contact_email = params.fetch("query_contact_email")
     the_hotel.contacted = params.fetch("query_contacted", false)
     the_hotel.chosen = params.fetch("query_chosen", false)
     the_hotel.block_discount = params.fetch("query_block_discount", false)
     the_hotel.minimum_rooms = params.fetch("query_minimum_rooms")
-    the_hotel.neighborhood_id = params.fetch("query_neighborhood_id")
     the_hotel.price_options = params.fetch("query_price_options")
     the_hotel.notes = params.fetch("query_notes")
-    the_hotel.contact_name = params.fetch("query_contact_name")
-    the_hotel.final_price = params.fetch("query_final_price")
 
     if the_hotel.valid?
       the_hotel.save
@@ -68,19 +71,37 @@ class HotelsController < ApplicationController
     the_hotel = Hotel.where({ :id => the_id }).at(0)
 
     the_hotel.name = params.fetch("query_name")
-    the_hotel.website = params.fetch("query_website")
     the_hotel.address = params.fetch("query_address")
-    the_hotel.lat = params.fetch("query_lat")
-    the_hotel.lng = params.fetch("query_lng")
+
+        #lat & lng
+        if the_hotel.address != ""
+          maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + the_hotel.address + "&key=" + ENV.fetch("GMAPS_KEY")
+
+          resp = HTTP.get(maps_url)
+          raw_response = resp.to_s
+          parsed_response = JSON.parse(raw_response)
+    
+          results = parsed_response.fetch("results")
+          first_result = results.at(0)
+          geo = first_result.fetch("geometry")
+          loc = geo.fetch("location")
+    
+          the_hotel.lat = loc.fetch("lat")
+          the_hotel.lng = loc.fetch("lng")
+        end
+ 
+    neighborhood_name = params.fetch("query_neighborhood_name")
+    the_hotel.neighborhood_id = Neighborhood.where({ :name => neighborhood_name }).at(0).id
+
+    the_hotel.website = params.fetch("query_website")
+    the_hotel.contact_name = params.fetch("query_contact_name")
+    the_hotel.contact_email = params.fetch("query_contact_email")
     the_hotel.contacted = params.fetch("query_contacted", false)
     the_hotel.chosen = params.fetch("query_chosen", false)
     the_hotel.block_discount = params.fetch("query_block_discount", false)
     the_hotel.minimum_rooms = params.fetch("query_minimum_rooms")
-    the_hotel.neighborhood_id = params.fetch("query_neighborhood_id")
     the_hotel.price_options = params.fetch("query_price_options")
     the_hotel.notes = params.fetch("query_notes")
-    the_hotel.contact_name = params.fetch("query_contact_name")
-    the_hotel.final_price = params.fetch("query_final_price")
 
     if the_hotel.valid?
       the_hotel.save
