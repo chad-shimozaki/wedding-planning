e40baaf0d6526ea06ab01318edf97496f96bd95b
chad-shimozaki <chad.shimozaki@chicagobooth.edu>
2024-12-13 20:51:18 +0000

    Removing ability to change meaningless parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  # No additional parameters needed at this time
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :avatar_url])
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :avatar_url])
 
-    devise_parameter_sanitizer.permit(:account_update, :keys => [:avatar_url])
-  end
+  #   devise_parameter_sanitizer.permit(:account_update, :keys => [:avatar_url])
+  # end
 end
